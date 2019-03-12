8f1d88b456eef356d69faad3bd88635f58bb1a8d
kirylmisiuk <kirill.misiuk@gmail.com>
2019-03-12 17:57:43 +0300

    added tesk2

--- tasks/homework_1/task2.js
+++ tasks/homework_1/task2.js
@@ -1,8 +1,12 @@
 let result = false;
-const duplicateArgs = arr => ...arr.split(' ').forEach((item, i, arr) =>  result = i !== arr.indexOf(item) || i !== arr.lastIndexOf(item) );
-duplicateArgs([1,2,3]);
+const duplicateArgs = (...arr) => arr.forEach((item, i, arr) =>  result = i !== arr.indexOf(item) || i !== arr.lastIndexOf(item));
+duplicateArgs(1,2,3);
 console.log(result);
-duplicateArgs([35, 14, 40, 14]);
+duplicateArgs(35,14,40,14);
 console.log(result);
-duplicateArgs(['abc', 'sso', 'js', 'true', 'else', 'js']);
+duplicateArgs('18','2','77','2');
 console.log(result);
+duplicateArgs('abc', 'sso', 'js', 'true', 'else', 'js');
+console.log(result);
+
+
