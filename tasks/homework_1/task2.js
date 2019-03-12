9476c4caa89e375af83f4e1fd21d6d82cd1709cf
kirylmisiuk <kirill.misiuk@gmail.com>
2019-03-12 19:20:27 +0300

    final commit

--- tasks/homework_1/task2.js
+++ tasks/homework_1/task2.js
@@ -1,11 +1,11 @@
 let result = false;
 const duplicateArgs = (...array) => {
-    array.forEach((item, itemNumber, array) =>  result = itemNumber !== array.indexOf(item) || itemNumber !== array.lastIndexOf(item));
+    array.forEach((item, itemNumber, array) => result = itemNumber !== array.indexOf(item) || itemNumber !== array.lastIndexOf(item));
     console.log(result);
-    };
-duplicateArgs(1,2,3);
-duplicateArgs(35,14,40,14);
-duplicateArgs('18','2','77','2');
+};
+duplicateArgs(1, 2, 3);
+duplicateArgs(35, 14, 40, 14);
+duplicateArgs('18', '2', '77', '2');
 duplicateArgs('abc', 'sso', 'js', 'true', 'else', 'js');
 
 
--- tasks/homework_1/task3.js
+++ tasks/homework_1/task3.js
@@ -2,22 +2,22 @@ const clients = [{
     name: 'Филип Фрай',
     email: 'fray@mail.un',
     isSubscribed: false,
-    orders: [ 11700, 1980, 450, 5500 ]
+    orders: [11700, 1980, 450, 5500]
 }, {
     name: 'Бендер Сгибатель Родригес',
     email: 'bender.rodriges@rambler.un',
     isSubscribed: true,
-    orders: [ 440, 226, 7650, 2990, 70 ]
+    orders: [440, 226, 7650, 2990, 70]
 }, {
     name: 'Доктор Джон Зоидберг',
     email: 'zoidberg-md@list.un',
     isSubscribed: true,
-    orders: [ 720 ]
+    orders: [720]
 }];
-const chageName = (clients,clientNumber,newName) =>{
-const copy = Object.assign({},clients);
-copy[clientNumber-1].name=newName;
-return copy;
+const chageName = (clients, clientNumber, newName) => {
+    const copy = Object.assign({}, clients);
+    copy[clientNumber - 1].name = newName;
+    return copy;
 };
 console.log(clients);
-console.log('\n',chageName(clients,2,'Мартин Скорсезе'))
+console.log('\n', chageName(clients, 2, 'Мартин Скорсезе'));
