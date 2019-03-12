96d917e2be4cf9fd2576999517442132ca48f6b0
kirylmisiuk <kirill.misiuk@gmail.com>
2019-03-12 19:17:51 +0300

    final commit

--- tasks/homework_1/task2.js
+++ tasks/homework_1/task2.js
@@ -1,12 +1,11 @@
 let result = false;
-const duplicateArgs = (...arr) => arr.forEach((item, i, arr) =>  result = i !== arr.indexOf(item) || i !== arr.lastIndexOf(item));
+const duplicateArgs = (...array) => {
+    array.forEach((item, itemNumber, array) =>  result = itemNumber !== array.indexOf(item) || itemNumber !== array.lastIndexOf(item));
+    console.log(result);
+    };
 duplicateArgs(1,2,3);
-console.log(result);
 duplicateArgs(35,14,40,14);
-console.log(result);
 duplicateArgs('18','2','77','2');
-console.log(result);
 duplicateArgs('abc', 'sso', 'js', 'true', 'else', 'js');
-console.log(result);
 
 
--- tasks/homework_1/task4.js
+++ tasks/homework_1/task4.js
@@ -1,1 +1,20 @@
+const clients = [{
+    name: 'Филип Фрай',
+    email: 'fray@mail.un',
+    isSubscribed: false,
+    orders: [ 11700, 1980, 450, 5500 ]
+}, {
+    name: 'Бендер Сгибатель Родригес',
+    email: 'bender.rodriges@rambler.un',
+    isSubscribed: true,
+    orders: [ 440, 226, 7650, 2990, 70 ]
+}, {
+    name: 'Доктор Джон Зоидберг',
+    email: 'zoidberg-md@list.un',
+    isSubscribed: true,
+    orders: [ 720 ]
+}];
+const mailing = (clients) => clients.filter((item) => item.isSubscribed===true).forEach((item) => console.log(`${item.name}, ваш заказ принят, подробности вашего заказа в письме, которое мы отправили на ${item.email}`));
+mailing(clients);
+
 
