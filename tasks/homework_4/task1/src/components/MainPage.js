375113ba2180103892e4536df77e67c02cbb05ee
kirylmisiuk <kirill.misiuk@gmail.com>
2019-03-21 19:53:47 +0300

    fist commit homework_4

---
+++ tasks/homework_4/task1/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainPage = ({}) => (
+    <div id="page-content">
+        <div id="inner-content">
+
+        </div>
+    </div>
+
+);
+MainPage.propTypes = {};
+export default MainPage
+
