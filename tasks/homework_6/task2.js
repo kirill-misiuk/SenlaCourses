a2f3180ee4e81d98392e6171d59ed7e4b063120e
kirylmisiuk <kirill.misiuk@gmail.com>
2019-04-07 23:31:39 +0300

    add hw_6

--- tasks/homework_6/task2.js
+++ tasks/homework_6/task2.js
@@ -1,7 +1,6 @@
-let x = (str) => {
-    if (typeof str === 'string') str=str.split('');
-   str= str.filter((v, i) => str.indexOf(v) === i);
-   return str
+const deleteDuplicate = arr => {
+    if (typeof arr === 'string') arr = arr.split('');
+    return arr.reduce((list, next) => list.slice(-1)[0] === next ? list : [...list, next], [])
 };
-console.log(x('AAAABBBCCDAABBB'));
-console.log(x([2,4,4,5,2,2,8]));
+console.log(deleteDuplicate('AAAABBBCCDAABBB'));
+console.log(deleteDuplicate([2, 4, 4, 5, 2, 2, 8]));
--- tasks/homework_6/task3.js
+++ tasks/homework_6/task3.js
@@ -1,14 +1,18 @@
 const getWave = x => {
     let arr = [];
     for (let i = 0; i < x.length; i++) {
-        let newstr;
-        if (x.charAt(i) === ' ') i++;
-        if (x.charAt(0).toUpperCase() === x.charAt(i))
-            x = x.substring(0, 0) + x.charAt(i).toLocaleLowerCase() + x.substring(1);
-        newstr = x.substring(0, i) + x.charAt(i).toLocaleUpperCase() + x.substring(i + 1);
-        arr = arr.concat(newstr);
+        if (x.charAt(i) !== ' ') {
+            let newstr;
+            if (x.charAt(0).toUpperCase() === x.charAt(i))
+                x = x.substring(0, 0) + x.charAt(i).toLocaleLowerCase() + x.substring(1);
+            newstr = x.substring(0, i) + x.charAt(i).toLocaleUpperCase() + x.substring(i + 1);
+            arr = arr.concat(newstr);
+        }
     }
     return arr
 };
 console.log(getWave('hello world'));
 console.log(getWave('React'));
+console.log(getWave('a       b    '));
+console.log(getWave(' gap '));
+console.log(getWave('  kuq'));
