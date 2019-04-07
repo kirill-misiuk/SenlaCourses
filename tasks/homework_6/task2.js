ac04356e411f1841707b6b2407de18ce69d35f61
kirylmisiuk <kirill.misiuk@gmail.com>
2019-04-07 23:34:47 +0300

    add hw_6

--- tasks/homework_6/task2.js
+++ tasks/homework_6/task2.js
@@ -1,6 +1,6 @@
 const deleteDuplicate = arr => {
     if (typeof arr === 'string') arr = arr.split('');
-    return arr.reduce((list, next) => list.slice(-1)[0] === next ? list : [...list, next], [])
+    return arr.reduce((arr, next) => arr.slice(-1)[0] === next ? arr : [...arr, next], [])
 };
 console.log(deleteDuplicate('AAAABBBCCDAABBB'));
 console.log(deleteDuplicate([2, 4, 4, 5, 2, 2, 8]));
