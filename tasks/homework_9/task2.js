c7e0805756bfd785158760f6be40ef95673b2587
kirylmisiuk <kirill.misiuk@gmail.com>
2019-04-19 09:46:54 +0300

    problem with task2

--- tasks/homework_9/task2.js
+++ tasks/homework_9/task2.js
@@ -1,3 +1,6 @@
+
+
+
 const getStatistic = (str) => {
     let arr;
     let result=[];
@@ -5,27 +8,32 @@ const getStatistic = (str) => {
     arr.forEach(item=>result.push(item.split('|')));
     return `Range: ${Range(result)} Average: ${Average(result)} Median: ${Median(result)}`
 };
-const Range = (arr) => {
-    let Array = [];
-    let arrayMin = [];
-    let arrayMax = [];
-    let arrayNum = [];
-    arr.forEach(item => Array.push(item.join().replace(/,/g, '').replace(/ /g, ', ')));
-    Array.forEach(item => arrayNum.push(+item));
-    let indexMax = arrayNum.indexOf(Math.max(...Array));
-    let indexMin = arrayNum.indexOf(Math.min(...Array));
-    arr[indexMax].forEach(item => arrayMax.push(+item));
-    arr[indexMin].forEach(item => arrayMin.push(+item));
-    if (arrayMax[2] < arrayMin[2]) {
-        arrayMax[1] = arrayMax[1] - 1;
-        arrayMax[2] = arrayMax[2] + 60;
-    }
-    if (arrayMax[1] < arrayMin[1]) {
-        arrayMax[0] = arrayMax[0] - 1;
-        arrayMax[1] = arrayMax[1] + 60;
-    }
-    return toString([arrayMax[0] - arrayMin[0], arrayMax[1] - arrayMin[1], arrayMax[2] - arrayMin[2]])
+const ddd=number=>{
+    if(String(number).length===1)return`0${number}`;
+    else return number
 };
+        const Range = (arr) => {
+            let Array = [];
+            let arrayMin = [];
+            let arrayMax = [];
+            let arrayNum = [];
+            arr.forEach(item => Array.push(item.join().replace(/,/g, '').replace(/ /g, ', ')));
+            Array.forEach(item => arrayNum.push(+item));
+            let indexMax = arrayNum.indexOf(Math.max(...Array));
+            let indexMin = arrayNum.indexOf(Math.min(...Array));
+            arr[indexMax].forEach(item => arrayMax.push(+item));
+            arr[indexMin].forEach(item => arrayMin.push(+item));
+            if (arrayMax[2] < arrayMin[2]) {
+                arrayMax[1] = arrayMax[1] - 1;
+                arrayMax[2] = arrayMax[2] + 60;
+            }
+            if (arrayMax[1] < arrayMin[1]) {
+                arrayMax[0] = arrayMax[0] - 1;
+                arrayMax[1] = arrayMax[1] + 60;
+            }
+            return toString([ddd(arrayMax[0] - arrayMin[0]), ddd(arrayMax[1] - arrayMin[1]), ddd(arrayMax[2] - arrayMin[2])])
+        };
+
 
 
 const Median = (arr) => {
@@ -52,7 +60,7 @@ const Median = (arr) => {
     else min = Math.round((minArr[(minArr.length) / 2] + minArr[(minArr.length) / 2 - 1]) / 2);
     if (hoursArr.length % 2 !== 0) hours = hoursArr[(hoursArr.length - 1) / 2];
     else hours = Math.round((hoursArr[(hoursArr.length) / 2] + hoursArr[(hoursArr.length) / 2 - 1]) / 2);
-    return toString([hours, min, sec])
+    return toString([ddd(hours), ddd(min), ddd(sec)])
 };
 
 const Average=(arr)=>{
@@ -66,30 +74,34 @@ const Average=(arr)=>{
         hoursArr.push(+arr[i][0]);
     }
 let secSum,minSum,hoursSum;
-    console.log(secSum=secArr.reduce((sum,current)=>sum+current));
-    console.log(minSum=minArr.reduce((sum,current)=>sum+current));
     console.log(hoursSum=hoursArr.reduce((sum,current)=>sum+current));
+    console.log(minSum=minArr.reduce((sum,current)=>sum+current));
+    console.log(secSum=secArr.reduce((sum,current)=>sum+current));
 
 
-hoursSum=Math.round(hoursSum/arr.length);
+    if(secSum>=60) {
+        sec=Math.floor(secSum / 60);
+        secSum = secSum-(sec*60);
+        minSum=minSum+sec
+    }
+    if(minSum>=60){
+        min=Math.floor(minSum/60);
+        minSum=minSum-(min*60);
+        hoursSum=hoursSum+min;
+    }
+    hoursSum=Math.floor(hoursSum/arr.length);
+    minSum=Math.floor(minSum/arr.length);
+    secSum=Math.floor(secSum/arr.length);
 
-if(secSum>=60){
-secSum=Math.round((secSum-(Math.floor(secSum/60)*60))/arr.length);
-minSum=minSum+Math.floor(secSum/60);
-}else{
-    secSum=Math.round(secSum/arr.length);
-}
-if(minSum>=60){
-    minSum=Math.round((minSum-(Math.floor(minSum/60)*60))/arr.length);
-    hoursSum=hoursSum+Math.floor(minSum/60);
-}
-else minSum=Math.round(minSum/arr.length);
-    return toString([hoursSum,minSum,secSum])
+
+    return toString([ddd(hoursSum), ddd(minSum), ddd(secSum)])
 };
 
 const toString=(arr)=>{
     return arr.join(' ').replace(/ /g, '|');
 };
 console.log(getStatistic('12|17|20, 10|16|16, 1|22|00, 2|32|34, 10|26|37, 13|19|34, 2|32|34, 12|17|20, 02|17|20')) ;
+// console.log(getStatistic('1|32|34, 2|22|00, 2|17|17, 1|17|48, 02|17|20, 1|47|16, 00|22|34, 00|22|34, 1|47|16, 9|22|34, 2|17|17, 2|22|00, 00|17|20, 00|22|34, 10|26|37, 1|47|16, 11|22|00'));
+// console.log(getStatistic('9|22|34, 2|17|17, 12|17|48, 1|15|59, 10|16|16'));
 console.log(getStatistic('1|32|34, 2|22|00, 2|17|17, 1|17|48, 02|17|20, 1|47|16, 00|22|34, 00|22|34, 1|47|16, 9|22|34, 2|17|17, 2|22|00, 00|17|20, 00|22|34, 10|26|37, 1|47|16, 11|22|00'));
-console.log(getStatistic('9|22|34, 2|17|17, 12|17|48, 1|15|59, 10|16|16'));
+console.log(getStatistic('00|19|34, 02|17|20, 2|47|16'));
